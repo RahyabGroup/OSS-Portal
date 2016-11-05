2d1343c222391df4c47d8de045ccc894e4f3cd7f
farshad-sharif <farshadsharifjob@gmail.com>
2016-11-05 17:49:47 +0330

    surplusage codes cleared

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -16,38 +16,8 @@ class pagesController extends Controller
 	{
 		return view('404');
 	}
-    public function alwayMenu()
-	{
-		return view('alwayMenu');
-	}
-    public function alwayMenuIn()
-	{
-		return view('alwayMenuIn');
-	}
-    public function blankPage()
-	{
-		return view('blankPage');
-	}
-    public function calendar()
-	{
-		return view('calendar');
-	}
-    public function chartFlot()
-	{
-		return view('chartFlot');
-	}
-    public function chartMorris()
-	{
-		return view('chartMorris');
-	}
-    public function chartOther()
-	{
-		return view('chartOther');
-	}
-    public function CloseRightSlider()
-	{
-		return view('CloseRightSlider');
-	}
+    
+   
     public function aroosha_oss()
 	{
 		return view('aroosha_oss');
@@ -56,194 +26,24 @@ class pagesController extends Controller
 	{
 		return view('dashboard2');
 	}
-    public function drop_upload()
-	{
-		return view('drop_upload');
-	}
-    public function fileManager()
-	{
-		return view('fileManager');
-	}
-    public function footerMenu()
-	{
-		return view('footerMenu');
-	}
-    public function footerShow()
-	{
-		return view('footerShow');
-	}
-    public function form()
-	{
-		return view('form');
-	}
-    public function form_ajax()
-	{
-		return view('form_ajax');
-	}
-    public function form_x_edit()
-	{
-		return view('form_x_edit');
-	}
-    public function formComponents()
-	{
-		return view('formComponents');
-	}
-    public function formMutiselect()
-	{
-		return view('formMutiselect');
-	}
-    public function formValidate()
-	{
-		return view('formValidate');
-	}
-    public function formWizard()
-	{
-		return view('formWizard');
-	}
-    public function hideUserPanel()
-	{
-		return view('hideUserPanel');
-	}
-    public function hideUserPanelIn()
-	{
-		return view('hideUserPanelIn');
-	}
-    public function images_manager()
-	{
-		return view('images_manager');
-	}
+    
+   
     public function index()
 	{
 		return view('index');
 	}
-    public function load_img()
-	{
-		return view('load_img');
-	}
-    public function lockscreen()
-	{
-		return view('lockscreen');
-	}
+   
     public function login()
 	{
 		return view('login');
 	}
-    public function mailBox()
-	{
-		return view('mailBox');
-	}
-    public function map()
-	{
-		return view('map');
-	}
-    public function menu()
-	{
-		return view('menu');
-	}
-    public function menuReal()
-	{
-		return view('menuReal');
-	}
-    public function menuVertical()
-	{
-		return view('menuVertical');
-	}
-    public function page_invoice()
-	{
-		return view('page_invoice');
-	}
-    public function page_search()
-	{
-		return view('page_search');
-	}
-    public function pages_pricing()
-	{
-		return view('pages_pricing');
-	}
-    public function profile()
-	{
-		return view('profile');
-	}
+ 
     public function register()
 	{
 		return view('register');
 	}
-    public function showhide()
-	{
-		return view('showhide');
-	}
-    public function showRightPanel()
-	{
-		return view('showRightPanel');
-	}
-    public function siteMap()
-	{
-		return view('siteMap');
-	}
-    public function slide()
-	{
-		return view('slide');
-	}
-    public function sliderIntro()
-	{
-		return view('sliderIntro');
-	}
-    public function table()
-	{
-		return view('table');
-	}
-    public function tableResponsive()
-	{
-		return view('tableResponsive');
-	}
-    public function tagInput()
-	{
-		return view('tagInput');
-	}
-    public function timeline()
-	{
-		return view('timeline');
-	}
-    public function topMenu()
-	{
-		return view('topMenu');
-	}
-    public function ui()
-	{
-		return view('ui');
-	}
-    public function ui_alert()
-	{
-		return view('ui_alert');
-	}
-    public function ui_button()
-	{
-		return view('ui_button');
-	}
-    public function ui_icon()
-	{
-		return view('ui_icon');
-	}
-    public function ui_modal()
-	{
-		return view('ui_modal');
-	}
-    public function ui_nestable()
-	{
-		return view('ui_nestable');
-	}
-    public function ui_panel()
-	{
-		return view('ui_panel');
-	}
-    public function ui_slide()
-	{
-		return view('ui_slide');
-	}
-    public function ui_typography()
-	{
-		return view('ui_typography');
-	}
+   
+   
 	public function switch_lang()
 	{
 		return view('switch_lang');
@@ -288,9 +88,9 @@ public function dashboardFour()
 	{
 		return view('dashboard5');
 	}
-	public function dashboardSix()
+	public function my_tickets()
 	{
-		return view('dashboard6');
+		return view('my_tickets');
 	}
 	public function dashboardSeven()
 	{
@@ -300,10 +100,7 @@ public function dashboardFour()
 	{
 		return view('dashboard8');
 	}
-	public function hello()
-	{
-		return view('hello');
-	}
+	
 	public function dashboardTest()
 	{
 		return view('dashboardTest');
